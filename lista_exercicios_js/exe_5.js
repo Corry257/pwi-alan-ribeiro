3bcd1b372038731f8b1c47cc4569595acf80c9b2
Corry257 <145715803+Corry257@users.noreply.github.com>
2024-11-23 04:09:08 -0300

    Resolução - exercício 5

--- lista_exercicios_js/exe_5.js
+++ lista_exercicios_js/exe_5.js
@@ -1,4 +1,9 @@
 /* 5 - Faça um algoritmo que leia o valor do salário mínimo e o valor do salário de um usuário, calcule quantos salários mínimos esse usuário ganha e imprima na tela o resultado.
  (Base para o Salário mínimo R$ 1.293,20). */
 
- 
+ let salario_minimo, salario_usuario
+
+salario_minimo = 1293,20
+salario_usuario = 4000,00
+
+console.log("O usuário ganha",salario_usuario / salario_minimo,"salario(s) mínimo(s)"
